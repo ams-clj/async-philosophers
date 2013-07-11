79c07db71e7f1e34e0a357e6ee02d0ba0e72dd36
Pepijn de Vos <pepijndevos@gmail.com>
2013-07-11 11:21:28 +0200

    working code

--- src/async_test/core.clj
+++ src/async_test/core.clj
@@ -1,6 +1,87 @@
-(ns async-test.core)
+(ns async-test.core
+  (:use clojure.core.async))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def philosophers {:socrates   [0 1]
+                   :plato      [1 2]
+                   :hickey     [2 3]
+                   :sussman    [3 4]
+                   :stroustrup [4 0]})
+
+(defn fork [ch]
+  (>!! ch :fork)
+  (<!! ch)
+  (recur ch))
+
+(defn set-table []
+  (vec
+    (for [ch [(chan) (chan) (chan) (chan) (chan)]]
+      (do
+        (thread (fork ch))
+        ch))))
+
+(defn make-butler []
+  (chan (dec (count philosophers))))
+
+(def table (set-table))
+(def butler (make-butler))
+
+; just for debugging
+; channels are flow, not state
+(def philostate (atom {:socrates   :thinking
+                       :plato      :thinking
+                       :hickey     :thinking
+                       :sussman    :thinking
+                       :stroustrup :thinking}))
+
+(def pickup <!!)
+(def putdown #(>!! % :fork))
+
+(def sit-down #(>!! % :sit))
+(def get-up <!!)
+
+(defn wait [] (Thread/sleep (+ 1000 (rand-int 5000))))
+
+(defn pickup-left [table philo]
+  (pickup (table (first (philo philosophers)))))
+
+(defn pickup-right [table philo]
+  (pickup (table (second (philo philosophers)))))
+
+(defn putdown-left [table philo]
+  (putdown (table (first (philo philosophers)))))
+
+(defn putdown-right [table philo]
+  (putdown (table (second (philo philosophers)))))
+
+(defn eat [table philo]
+  (pickup-left table philo)
+  (swap! philostate assoc philo :left-fork)
+  (println @philostate)
+  (wait)
+
+  (pickup-right table philo)
+  (swap! philostate assoc philo :eating)
+  (println @philostate)
+  (wait)
+
+  (putdown-right table philo)
+  (putdown-left table philo))
+
+(defn philosopher [table butler philo]
+  (wait)
+
+  (sit-down butler)
+  (swap! philostate assoc philo :sitting)
+  (println @philostate)
+
+  (eat table philo)
+
+  (get-up butler)
+  (swap! philostate assoc philo :thinking)
+  (println @philostate)
+  
+  (recur table butler philo))
+
+(defn run []
+  (doseq [p (keys philosophers)]
+    (thread (philosopher table butler p))))
--- test/async_test/core_test.clj
+++ test/async_test/core_test.clj
@@ -1,65 +1,16 @@
 (ns async-test.core-test
-  (:use midje.sweet)
-  (:use clojure.core.async))
-
-
-
-(defn set-table []
-  (let [table [(chan) (chan) (chan) (chan) (chan)]]
-    (go (>! (table 0) :fork))
-    (go (>! (table 1) :fork))
-    (go (>! (table 2) :fork))
-    (go (>! (table 3) :fork))
-    (go (>! (table 4) :fork))
-    table))
-(def table (set-table))
-(def philosophers {:socrates   [0 1]
-                   :plato      [1 2]
-                   :rich       [2 3]
-                   :sussman    [3 4]
-                   :stroustrup [4 0]})
-
-(defn pickup [fork]
-  (<! fork) (println :smth_more_easy_to_read))
-
-(defn pickup-left [table philo]
-  (pickup (table (first (philo philosophers)))))
-
-(defn pickup-right [table philo]
-  (pickup (table (second (philo philosophers)))))
-
-(defn putdown [fork]
-  (>! fork :fork))
-
-(defn putdown-left [table philo]
-  (putdown (table (first (philo philosophers)))))
-
-(defn putdown-right [table philo]
-  (putdown (table (second (philo philosophers)))))
-
-(defn finish-eating [table philo]
-  (putdown-right table philo)
-  (putdown-left table philo))
-
-(defn eat [table philo]
-  (go  (pickup-left table philo)
-       ;; check I have the fork
-       (pickup-right table philo)
-       ;; check philo has the forks!!!!
-       (println "nomnomnom" philo))
-;  (putdown-right table philo)
-;  (putdown-left table philo)
-  )
+  (:use async-test.core)
+  (:use midje.sweet))
 
 (fact "I'm broken"
-  ;; (<!! (pickup ((set-table) 0))) => :fork
-  ;; (<!! (let [t (set-table)]
-  ;;        (pickup (t 0))
-  ;;        (putdown (t 0))
-  ;;        (pickup (t 0)))) => :fork
-  ;; (eat (set-table) :plato) => identity
-  ;; (println "test tarts here")
+  (pickup ((set-table) 0)) => :fork
   (let [t (set-table)]
-    (doall (map (fn [philo] (eat t philo)) (keys philosophers))))
-  => :false
+     (pickup (t 0))
+     (putdown (t 0))
+     (pickup (t 0))) => :fork
+  (eat (set-table) :plato) => nil
+  (let [t (set-table)]
+    (for [p (rest (keys philosophers))]
+      (thread (eat t p))))
+  => #(doall (map <!! %))
 )
